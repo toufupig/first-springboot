82da44a391bb45e0011dcaa04a41c9e3409e5358
toufupig <1406380135@qq.com>
2020-05-05 17:42:29 +0800

    添加输入字符串校验

--- src/main/java/com/toufupig/firstspringboot/controller/TestController.java
+++ src/main/java/com/toufupig/firstspringboot/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.toufupig.firstspringboot.controller;
 
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("/test")
@@ -11,7 +14,11 @@ public class TestController {
 
     @GetMapping("/aaa")
     public String test01(@RequestParam(required = true) String aaa){
-
+        String regix = "[0-9]*";
+        boolean matches = Pattern.matches(regix, aaa);
+        if (matches) {
+            return "输入的字符串不能都是数字！";
+        }
         return aaa;
     }
 
