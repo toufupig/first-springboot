f3c4da331123fa4ef2f259536e46fe2397dd1fe5
toufupig <1406380135@qq.com>
2020-05-05 18:05:38 +0800

    添加学生实体类

--- src/main/java/com/toufupig/firstspringboot/controller/TestController.java
+++ src/main/java/com/toufupig/firstspringboot/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.toufupig.firstspringboot.controller;
 
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
---
+++ src/main/java/com/toufupig/firstspringboot/model/Student.java
@@ -0,0 +1,12 @@
+package com.toufupig.firstspringboot.model;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private String name;
+    private Integer age;
+    private String address;
+
+}
